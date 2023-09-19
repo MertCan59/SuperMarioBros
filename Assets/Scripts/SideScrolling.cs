a89545d9ee19253f12b6fe42402e58970afb0ee0
Mert can köymen <mertcankoymen@gmail.com>
2023-09-19 19:58:15 +0300

    Will be continued

---
+++ Assets/Scripts/SideScrolling.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition=transform.position;
+        cameraPosition.x=Mathf.Max(cameraPosition.x,player.position.x);
+        transform.position=cameraPosition;
+    }
+}
